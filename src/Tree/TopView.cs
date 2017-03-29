a80dd1d2dcbfe2ec0ff882106e49b143a43d6242
seifipour <fa.seifipour@gmail.com>
2017-03-29 21:10:34 +0100

    top view node tree

---
+++ src/Tree/TopView.cs
@@ -0,0 +1,84 @@
+﻿using CrackingCode.src.Tree.lib;
+using System.Collections;
+
+namespace CrackingCode.src.Tree
+{
+    public static  class TopView
+    {
+       public static string print_top_view_nodes_with_while_solution(Tree<int> root) {
+
+            var root_right = root;
+            var root_left = root;
+
+            var result_left =new  Stack();
+            var result = string.Empty;
+
+            if (root == null) return result;
+          
+            while (root_left.left != null)
+            {
+                result_left.Push(root_left.left.data);
+                root_left = root_left.left;
+            }
+
+            while (result_left.Count != 0)
+            {
+                result += result_left.Pop() +" ";
+            }
+
+            result += root.data +" ";
+
+            while (root_right.right != null)
+            {
+                result += root_right.right.data+" ";
+                root_right = root_right.right;
+            }
+
+            return result;
+        }
+
+        /*         50
+        *         /   \
+        *        30    60
+        *       /  \   / \
+        *      20  40 55  70
+        *                  \ 
+        *                  80
+        */
+        public static string print_top_view_nodes_with_recursion(Tree<int> root)
+        {
+            var result = string.Empty;
+
+            if (root == null) return result;
+
+            var left_side = side_value_for_left(root.left);
+            var right_side = side_value_for_right(root.right);
+
+            result += left_side +root.data + " " + right_side;
+
+            return result;
+        }
+
+        public static string side_value_for_left(Tree<int> root) {
+
+            string result = string.Empty;
+            if (root == null) return result;
+            result = side_value_for_left(root.left);
+            result += root.data + " ";
+
+            return result;
+        }
+
+        public static string side_value_for_right(Tree<int> root)
+        {
+
+            string result = string.Empty;
+            if (root == null) return result;
+
+            result += root.data + " ";
+            result += side_value_for_right(root.right);
+
+            return result;
+        }
+    }
+}
---
+++ test/TreeTest/TopViewTest.cs
@@ -0,0 +1,57 @@
+﻿using CrackingCode.src.Tree;
+using CrackingCode.src.Tree.lib;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CrackingCode.test.TreeTest
+{
+    [TestClass]
+    public class TopViewTest
+    {
+        [TestMethod]
+        public void preint_the_top_view_node_with_while_sulotion()
+        {
+            //arrange
+
+            var root = new Tree<int>(50);
+            var BST = new BinarySearchTree(root);
+            BST.insert(60);
+            BST.insert(55);
+            BST.insert(70);
+            BST.insert(80);
+            BST.insert(30);
+            BST.insert(40);
+            BST.insert(20);
+            //act
+            var result = TopView.print_top_view_nodes_with_while_solution(BST.root);
+                       
+
+            var expected_result = "20 30 50 60 70 80 ";
+            //assert
+            Assert.AreEqual(expected_result, result);
+        }
+
+        [TestMethod]
+        public void preint_the_top_view_node_with_recursion_solution()
+        {
+            //arrange
+
+            var root = new Tree<int>(50);
+            var BST = new BinarySearchTree(root);
+            BST.insert(60);
+            BST.insert(55);
+            BST.insert(70);
+            BST.insert(80);
+            BST.insert(30);
+            BST.insert(40);
+            BST.insert(20);
+            //act
+            var result = TopView
+                        .print_top_view_nodes_with_recursion(BST.root);
+
+
+            var expected_result = "20 30 50 60 70 80 ";
+            //assert
+            Assert.AreEqual(expected_result, result);
+        }
+    }
+}
