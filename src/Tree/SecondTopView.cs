6486de968566f303ea5774159bd2b901034fdab0
seifipour <fa.seifipour@gmail.com>
2017-03-31 16:21:53 +0100

    cleen up on second top view

--- src/Tree/SecondTopView.cs
+++ src/Tree/SecondTopView.cs
@@ -1,26 +1,25 @@
 ﻿using CrackingCode.src.Tree.lib;
-using System;
 using System.Collections.Generic;
 using System.Text;
 using CodeCrack.src.linkedlist;
 
 namespace CrackingCode.src.Tree
 {
-   /*         50*  (4)
-   *         /    \
-   *   (3) *30    60* (5)
-   *       /  \   /  \
-   * (2) *20  40 55  70*(6)
-   *        \     \    \ 
-   *       27      56  80* (7)
-   *       /        \
-   *      22         57
-   *     /            \
-   *(1)*21              58
-   *                    \ 
-   *                    59* (8)
-   *  expected result string result = "21 20 30 50 60 70 80 59"                  
-   */
+    /*         50*  (4)
+    *         /    \
+    *   (3) *30    60* (5)
+    *       /  \   /  \
+    * (2) *20  40 55  70*(6)
+    *        \     \    \ 
+    *       27      56  80* (7)
+    *       /        \
+    *      22         57
+    *     /            \
+    *(1)*21              58
+    *                    \ 
+    *                    59* (8)
+    *  expected result string result = "21 20 30 50 60 70 80 59"                  
+    */
     public class SecondTopView
     {
         public static string print_top_view_nodes_with_return_recursion(Tree<int> root)
@@ -30,18 +29,13 @@ namespace CrackingCode.src.Tree
             if (root == null) return result.ToString();
 
             var left_side = side_value_for_left_forward_recursion(
-                                 root: root.left 
-                                ,max: 0
-                                ,current:1
-                                ,total_result:new CodeCrack.src.linkedlist.LinkedList<int>()
-                            );
+                                  root: root.left
+                                 , sides: new Sides());
 
             var right_side = side_value_for_right_return_recurstion(
-                                root:root.right
-                                ,max: new max()
-                                ,current: 1
-                                ,total_result: new CodeCrack.src.linkedlist.LinkedList<int>()
-                             );
+                                  root: root.right
+                                , sides: new Sides());
+
             var new_tail = new Node<int>(root.data);
             left_side.tail.next = new_tail;
             left_side.tail = new_tail; 
@@ -59,58 +53,57 @@ namespace CrackingCode.src.Tree
 
         private static CodeCrack.src.linkedlist.LinkedList<int> side_value_for_left_forward_recursion
                                                 (Tree<int> root
-                                                , int max
-                                                , int current
-                                                , CodeCrack.src.linkedlist.LinkedList<int> total_result)
+                                                , Sides sides)
         {
-            if (root == null) return total_result;
+            if (root == null) { sides.current -= 1; return sides.total_result; }
 
-            if (current > max)
+            if (sides.current > sides.max)
             {
-                if (total_result.head == null)
+                if (sides.total_result.head == null)
                 {
-                    total_result.head = new Node<int>(root.data);
-                    total_result.tail = total_result.head;
+                    sides.total_result.head = new Node<int>(root.data);
+                    sides.total_result.tail = sides.total_result.head;
                 }
                 else {
                     var head= new Node<int>(root.data);
-                    var temp= total_result.head;
+                    var temp= sides.total_result.head;
                     head.next = temp;
-                    total_result.head = head;
+                    sides.total_result.head = head;
 
                 }
-                max = current;
+                sides.max = sides.current;
             }
-            side_value_for_left_forward_recursion(root.left,max, current+1, total_result);
+            sides.current += 1;
+            side_value_for_left_forward_recursion(root.left, sides);
 
-            side_value_for_left_forward_recursion(root.right,max,current-1, total_result);
+            sides.current -= 1;
+            side_value_for_left_forward_recursion(root.right, sides);
 
-            return total_result;
+            return sides.total_result;
         }
         
 
         private static CodeCrack.src.linkedlist.LinkedList<int> side_value_for_right_return_recurstion
                                                (Tree<int> root
-                                                , max max
-                                                , int current
-                                                , CodeCrack.src.linkedlist.LinkedList<int> total_result)
-        {
-            if (root == null) return total_result;
+                                                , Sides sides)
+        { 
+            if (root == null) { return sides.total_result; };
 
-            if (current > max._max)
+            if (sides.current > sides.max)
             {
-                total_result.append(root.data);
+                sides.total_result.append(root.data);
 
-                max._max = current;
+                sides.max = sides.current;
             }
+            if (root.right != null)
+            {
+                sides.current += 1;
+            }
+            side_value_for_right_return_recurstion(root.right,sides);
+            sides.current -= 1;
+            side_value_for_right_return_recurstion(root.left,sides);
 
-            side_value_for_right_return_recurstion(root.right, max, current + 1, total_result);
-           
-            side_value_for_right_return_recurstion(root.left, max, current - 1, total_result);
-
-           
-
-            return total_result;
+            return sides.total_result;
         }
 
         private static List<int> side_value_for_left_return_recursion
@@ -143,12 +136,16 @@ namespace CrackingCode.src.Tree
 
     }
 
-    public class max {
+    public class Sides {
 
-        public int _max { set; get; }
-        public max()
+        public int max { set; get; }
+        public int current { get; set; }
+        public CodeCrack.src.linkedlist.LinkedList<int> total_result { get; set; }
+        public Sides()
         {
-            _max = 0;
+            max = 0;
+            current = 1;
+            total_result = new CodeCrack.src.linkedlist.LinkedList<int>();
         }
        
     }
