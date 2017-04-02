fe9a1c2c28ae00631fca7a4f0204a25fbb723861
seifipour <fa.seifipour@gmail.com>
2017-04-02 21:36:40 +0100

    level order traversal

---
+++ src/Tree/LevelOrderTraversal.cs
@@ -0,0 +1,53 @@
+﻿using CrackingCode.src.Tree.lib;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CrackingCode.src.Tree
+{
+    public static class LevelOrderTraversal
+    {
+       /*         50          level 1
+       *         /   \
+       *        30   60       level 2
+       *       /  \    \
+       *      20  40    70    level 3
+       *                  \ 
+       *                  80* level 4 
+       *                  
+       *  expected result "50 30 60 20 40 70 80"                
+       */
+        public static string print_level_order_traversal(Tree<int> root)
+        {
+            string result = "";
+
+            if (root == null) return result;
+            
+            var set_result = levelOrderLeft(root,1, new Dictionary<int, List<int>>());
+
+            set_result.OrderBy(x => x.Key);
+
+            set_result
+            .Values
+            .ToList()
+            .ForEach(each_key => each_key.ForEach(each_value => result += each_value + " "));
+
+            return result; 
+        }
+        public static Dictionary<int,List<int>> levelOrderLeft(Tree<int> root
+                                                              , int level 
+                                                              , Dictionary<int , List<int>> result)
+        {
+            if (root == null) return result;
+
+            if (result.Count < level) {
+                result.Add(level, new List<int>());
+            }
+            result.FirstOrDefault(x => x.Key == level).Value.Add(root.data);
+            result = levelOrderLeft(root.left,level+1, result);
+            result=  levelOrderLeft(root.right,level+1,result);
+            
+            return result;
+        }
+    }
+
+}
---
+++ test/TreeTest/LevelOrderTraversalTest.cs
@@ -0,0 +1,33 @@
+﻿using CrackingCode.src.Tree;
+using CrackingCode.src.Tree.lib;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CrackingCode.test.TreeTest
+{
+    [TestClass]
+    public class LevelOrderTraversalTest
+    {
+        [TestMethod]
+        public void print_level_order_traversal()
+        {
+            //arrange
+            var root = new Tree<int>(50);
+            var BST = new BinarySearchTree(root);
+            BST.insert(60);
+            BST.insert(70);
+            BST.insert(80);
+            BST.insert(30);
+            BST.insert(40);
+            BST.insert(20);
+            //act
+
+            var result =  LevelOrderTraversal.print_level_order_traversal(BST.root);
+
+            var expected_result ="50 30 60 20 40 70 80 ";
+
+            //assert
+            Assert.AreEqual(result, expected_result);
+
+        }
+    }
+}
