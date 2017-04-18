96f33513ccd245c5e0e4aecc22a5f8a4c5f357f9
seifipour <fa.seifipour@gmail.com>
2017-04-18 13:09:09 +0100

    Create a linkedlist for each Binary search tree depth.

---
+++ src/Tree/CreateALinkedListForEachBinarySearchTreeDepth.cs
@@ -0,0 +1,58 @@
+﻿using CrackingCode.src.Tree.lib;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace CrackingCode.src.Tree
+{
+    public static class CreateALinkedListForEachBinarySearchTreeDepth
+    {
+        public static Dictionary<int, CodeCrack.src.linkedlist.LinkedList<int>> return_list_of_linked_list_for_BST
+                                                                                                ( Tree<int> root ) {
+            if (root == null) return null;
+
+            return set_result(root, 1, new Dictionary<int, CodeCrack.src.linkedlist.LinkedList<int>>());
+        }
+
+        public static  Dictionary<int, CodeCrack.src.linkedlist.LinkedList<int>> set_result (
+                                                               Tree<int> root
+                                                             , int level
+                                                             , Dictionary<int, CodeCrack.src.linkedlist.LinkedList<int>> result) {
+
+            if (root == null) return result;
+
+            if (!result.ContainsKey(level))
+            {
+                result.Add(level, new CodeCrack.src.linkedlist.LinkedList<int>());
+            }
+
+          
+
+            if (result.FirstOrDefault(x => x.Key == level).Value.head != null)
+
+            {
+                while (result.FirstOrDefault(x => x.Key == level).Value.head.next != null)
+                {
+
+                    result.FirstOrDefault(x => x.Key == level).Value.head =
+                    result.FirstOrDefault(x => x.Key == level).Value.head.next;
+                }
+
+                result.FirstOrDefault(x => x.Key == level).Value.head.next =
+                new CodeCrack.src.linkedlist.Node<int>(root.data);
+            }
+            else {
+
+                result.FirstOrDefault(x => x.Key == level).Value.head =
+                new CodeCrack.src.linkedlist.Node<int>(root.data);
+
+            }
+
+            result = set_result(root.left, level+1,result);
+
+            result = set_result(root.right, level+1,result);
+
+            return result;
+        }
+    }
+} 
---
+++ test/TreeTest/CreateALinkedListForEachBinarySearchTreeDepthTest.cs
@@ -0,0 +1,51 @@
+﻿using CrackingCode.src.Tree;
+using CrackingCode.src.Tree.lib;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace CrackingCode.test.TreeTest
+{
+    [TestClass]
+    public class CreateALinkedListForEachBinarySearchTreeDepthTest
+    {
+        /*         50
+       *         /  \
+       *        30   60
+       *       /  \   \
+       *      20  40   70
+       *                \
+       *                 80
+       *              
+       */
+
+        [TestMethod]
+        public void check_balanced_tree()
+        {
+            //arrange
+            var root = new Tree<int>(50);
+            var BST = new BinarySearchTree(root);
+            BST.insert(60);
+            BST.insert(70);
+            BST.insert(80);
+            BST.insert(30);
+            BST.insert(40);
+            BST.insert(20);
+            //act
+
+            var result = CreateALinkedListForEachBinarySearchTreeDepth
+                        .return_list_of_linked_list_for_BST(BST.root);
+
+            var list_of_linked_list = new List<CodeCrack.src.linkedlist.LinkedList<int>>();
+
+            var num_of_depth = result.Keys.Count;
+            foreach (var each_value in result.Values)
+            {
+                list_of_linked_list.Add(each_value);
+            }
+
+            //assert
+            Assert.AreEqual(num_of_depth, list_of_linked_list.Count);
+
+        }
+    }
+}
