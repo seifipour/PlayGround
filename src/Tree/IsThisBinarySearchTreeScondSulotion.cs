84f3514a75dd893c15a614f5c6d5792110e69dbc
seifipour <fa.seifipour@gmail.com>
2017-04-27 21:54:24 +0100

    is this BST.

---
+++ src/Tree/IsThisBinarySearchTreeScondSulotion.cs
@@ -0,0 +1,41 @@
+﻿using CrackingCode.src.Tree.lib;
+
+namespace CrackingCode.src.Tree
+{
+    public static  class IsThisBinarySearchTreeScondSulotion
+    {
+        /*        50
+       *         /   \
+       *        30    60
+       *       /  \    \
+       *      20  40    70
+       *           \      \ 
+       *            48    80
+       *          /         \
+       *         45          90
+       */
+
+
+        public static bool is_this_binary_search_tree(Tree<int> root ) {
+
+            if (root == null) return false;
+
+            var min = int.MinValue;
+            var max = int.MaxValue;
+
+            var result = check_BST(root, min, max);
+           
+            return result;
+        }
+
+        public static bool check_BST(Tree<int> root, int min, int max ) {
+
+            if (root == null) return true;
+            if (root.data < min || root.data > max) {
+                return false; 
+            }
+            return check_BST(root.left, min, root.data)
+                    && check_BST(root.right, root.data, max);
+        }
+    }
+}
---
+++ test/TreeTest/IsThisBinarySearchTreeScondSulotionTest.cs
@@ -0,0 +1,46 @@
+﻿using CrackingCode.src.Tree;
+using CrackingCode.src.Tree.lib;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CrackingCode.test.TreeTest
+{
+    [TestClass]
+    public  class IsThisBinarySearchTreeScondSulotionTest
+    {
+        /*        50
+      *         /   \
+      *        30    60
+      *       /  \    \
+      *      20  40    70
+      *           \      \ 
+      *            48    80
+      *          /         \
+      *         45          90
+      */                   
+
+        [TestMethod]
+        public void is_this_a_binary_search_tree()
+        {
+            //arrange
+            var root = new Tree<int>(50);
+            var BST = new BinarySearchTree(root);
+            BST.insert(60);
+            BST.insert(70);
+            BST.insert(80);
+            BST.insert(90);
+            BST.insert(30);
+            BST.insert(40);
+            BST.insert(20);
+            BST.root.left.right.right = new Tree<int>(48);
+            BST.root.left.right.right.left = new Tree<int>(45);
+            //act
+
+            var result = IsThisBinarySearchTreeScondSulotion
+                        .is_this_binary_search_tree(BST.root);
+
+            //assert
+            Assert.AreEqual(result, false);
+
+        }
+    }
+}
